9f2a3aaf09eb6d4dec2024cab2d6fae79ecb05d0
Arif OKCAN <142164597+Etherfiend@users.noreply.github.com>
2024-11-20 17:42:24 +0300

    Update treesitter_configs.lua

--- lua/treesitter_configs.lua
+++ lua/treesitter_configs.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "c", "cpp"}, -- Dillerinizi buraya ekleyin
+    ensure_installed = { "c", "cpp"}, 
     highlight = {
         enable = true,              -- false ile devre dışı bırakabilirsiniz
         additional_vim_regex_highlighting = false,
